e9e0a0d3ac51c8c96d4e2ddee7c8e6494c1bd4ab
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-03-05 19:56:31 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -44,15 +44,15 @@ Board play;
 } //the unit test utilized for game
 
 void Game::oneTurn(Player* pp) {
-    string gameOptions;
+    int gameOptions;
 
-    cout << "Please pick one of the following options:\n" << endl;
-    cout << " Roll     Stop     Resign" << endl;
+    cout << "\nPlease pick one of the following options: " << endl;
+    cout << "1. Roll    2. Stop    3. Resign" << endl;
     cin >> gameOptions;
-    if (gameOptions == "Roll") {
+    if (gameOptions == 1) {
      Gameroll();
     }
-    else if (gameOptions == "Stop") {
+    else if (gameOptions == 2) {
         Gamestop();
     }
     else {
@@ -63,16 +63,16 @@ void Game::oneTurn(Player* pp) {
 
 void Game::Gameroll() {
     Dice* roll();
-    cout << "Please select one pair of dice: " << endl;
+    cout << "\nPlease select one pair of dice: " << endl;
     dices->roll();
     const int* diceArray = dices->roll();
     cout << "Output A: " << diceArray[0];
-    cout << "Output B: " << diceArray[1];
-    cout << "Output C: " << diceArray[2];
-    cout << "Output D: " << diceArray[3];
+    cout << "\nOutput B: " << diceArray[1];
+    cout << "\nOutput C: " << diceArray[2];
+    cout << "\nOutput D: " << diceArray[3];
     int firstVal;
     int secondVal;
-    cout << "Please select any of the dice values and letters: " << endl;
+    cout << "\nPlease select any of the dice values and letters: " << endl;
     cin >> firstVal;
     cin >> secondVal;
     int totalVal = (firstVal + secondVal);
@@ -91,3 +91,18 @@ void Game::Gamestop() {
     Play.stop();
     Player* wonColumn(int column);
 } //function utilized to run the stop option
+
+void Game::start() {
+    int endOptions;
+    oneTurn(player1);
+    cout << "Do you want to play again? " << endl;
+    cout << "1. Yes  2. No";
+    cin >> endOptions;
+    if (endOptions == 1) {
+        start();
+    }
+    else {
+        cout << "The game has been terminated!" << endl;
+        Play.stop();
+    }
+}
